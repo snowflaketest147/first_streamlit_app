e415797d62675192bfc7099d6adcc1e54357fcf4
snowflake test <122079136+snowflaketest147@users.noreply.github.com>
2023-01-06 12:18:02 +0530

    Update streamlit_app.py
    added breakfast and menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit
 
 streamlit.title('My healthy diner')
 
-streamlit.header('menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
