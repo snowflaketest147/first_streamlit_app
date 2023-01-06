4c9f69749d5fa895ad861ac8498dcc6a6362b463
snowflake test <122079136+snowflaketest147@users.noreply.github.com>
2023-01-06 12:19:46 +0530

    Update streamlit_app.py
    added new menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado toast')
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
