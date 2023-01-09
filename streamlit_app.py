46016bc9b552a13d5a1c98d2cf8fd6a3709cfca0
snowflake test <122079136+snowflaketest147@users.noreply.github.com>
2023-01-09 17:00:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+
+fruit_choice1 = streamlit.text_input('What fruit would you like to add')
+streamlit.write('The user entered ', fruit_choice1)
