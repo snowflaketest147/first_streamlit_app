d88fd1d1d182a42e3019382eb9bfb4358a10af74
snowflake test <122079136+snowflaketest147@users.noreply.github.com>
2023-01-09 17:08:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice1 = streamlit.text_input('What fruit would you like to add')
 streamlit.write('The user entered ', fruit_choice1)
-my_cur.execute("insert into fruit_load_list values('test')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
